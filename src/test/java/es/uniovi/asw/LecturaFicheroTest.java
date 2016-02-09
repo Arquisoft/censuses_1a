c2988bcc22b020fb4ec0413e6820fe6a508f9e9f
Carlos Villa <carlosvillablanco@MacBook-Pro-Carlos.local>
2016-02-09 14:31:25 +0100

    Test lectura fichero

---
+++ src/test/java/es/uniovi/asw/LecturaFicheroTest.java
@@ -0,0 +1,38 @@
+package es.uniovi.asw;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import es.uniovi.asw.logica.User;
+import es.uniovi.asw.parser.RCensus;
+import es.uniovi.asw.parser.ReadCensus;
+
+public class LecturaFicheroTest {
+	
+	@Test
+	public void evalLectura() {
+		ReadCensus readCensus = new RCensus("src/test/resources/Censos.xls");
+		readCensus.readCensus();
+		List<User> usuarios = readCensus.getUsuarios();
+		
+		User usuario1 = usuarios.get(0);
+		User usuario2 = usuarios.get(1);
+		
+		assertEquals(usuario1.getName(), "Jorge");
+		assertEquals(usuario2.getName(), "Miguel");
+		
+		assertEquals(usuario1.getNIF(), "45443827R");
+		assertEquals(usuario2.getNIF(), "54313432L");
+		
+		assertEquals(usuario1.getEmail(), "jorge@gmail.com");
+		assertEquals(usuario2.getEmail(), "miguel@gmail.com");
+		
+		assertEquals(usuario1.getCodigoMesa(), "1");
+		assertEquals(usuario2.getCodigoMesa(), "2");
+			
+	}
+
+}
