d0a767a3d8344009eca5282e10bcd13870078e99
Carlos Villa <carlosvillablanco@MacBook-Pro-Carlos.local>
2016-02-09 12:55:08 +0100

    Modificación clase User

--- src/main/java/es/uniovi/asw/logica/User.java
+++ src/main/java/es/uniovi/asw/logica/User.java
@@ -3,7 +3,57 @@ package es.uniovi.asw.logica;
 public class User {
 	
 	private String name;
-	private String correo;
+	private String NIF;
+	private String email;
+	private String codigoMesa;
+	
+
+	public User(String name,String NIF,String email,String codigoMesa){
+		this.setName(name);
+		this.setNIF(NIF);
+		this.email = email;
+		this.setCodigoMesa(codigoMesa);
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getNIF() {
+		return NIF;
+	}
+
+
+	public void setNIF(String nIF) {
+		NIF = nIF;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCodigoMesa() {
+		return codigoMesa;
+	}
+
+
+	public void setCodigoMesa(String codigoMesa) {
+		this.codigoMesa = codigoMesa;
+	}
+	
+	
 	
 
 }
