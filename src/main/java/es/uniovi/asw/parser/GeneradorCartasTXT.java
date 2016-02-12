9aff0462b776e3e4a0c10c3168a1d971d5607b80
Carlos Villa <uo236851@uniovi.es>
2016-02-12 21:52:24 +0100

    Extraído método generador contraseñas a clase propia

--- src/main/java/es/uniovi/asw/parser/GeneradorCartasTXT.java
+++ src/main/java/es/uniovi/asw/parser/GeneradorCartasTXT.java
@@ -4,16 +4,16 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Random;
+
+import es.uniovi.asw.util.GeneradorPasswords;
 
 public class GeneradorCartasTXT implements GeneradorCartas{
 
 	
-
 	@Override
 	public String generarCarta(String nombre,String email) {
 				
-		String contraseña = generarContraseña();
+		String contraseña = GeneradorPasswords.getGeneradorPasswords().generarContraseña();
 		
 		File file = new File("cartas/"+email+".txt");
 		BufferedWriter bW;
@@ -35,28 +35,4 @@ public class GeneradorCartasTXT implements GeneradorCartas{
 		
 		return contraseña;
 	}
-
-	private String generarContraseña() {
-		  char[] caracteres;
-		  String password = "";
-	        caracteres = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8'
-	        		, '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
-	        		'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-	        		'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-	        		'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
-	        		'u', 'v', 'w', 'x', 'y', 'z'};
-	        try {
-	            int repet = 10;
-	            for (int i = 0; i < repet; i++) {
-	            	password += caracteres[new Random().nextInt(62)];
-	            }
-	            
-	        } catch (NumberFormatException ex) {
-	            ex.printStackTrace(System.out);
-	        }
-	        
-	       return password; 
-	        
-	}
-
 }
