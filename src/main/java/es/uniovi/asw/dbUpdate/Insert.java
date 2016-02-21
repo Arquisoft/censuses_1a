0e8c645e98713ed3f889a8f15bea4cb2a5a76333
Carlos Villa <uo236851@uniovi.es>
2016-02-21 19:25:55 +0100

    Añadido miembro a POM

--- src/main/java/es/uniovi/asw/dbUpdate/Insert.java
+++ src/main/java/es/uniovi/asw/dbUpdate/Insert.java
@@ -7,9 +7,9 @@ import es.uniovi.asw.logica.User;
 public interface Insert {
 	
 	/**
-	 * Recibe un objeto User y lo inserta en la base de datos
+	 * Recibe una lista de objetos User y lo inserta en la base de datos
 	 * 
-	 * @param usuario
+	 * @param usuarios
 	 */
 	boolean insertarUsuarios(List<User> usuarios);
 	
